3bb6aa4ab840cece6682f57328785516a82364e0
10039538 <10039538@GSGZG8Q13D01LE>
2024-10-09 09:22:44 -0600

    Add|List

--- src/app/Componentes/listar-personas/listar-personas.component.ts
+++ src/app/Componentes/listar-personas/listar-personas.component.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiPersonaService } from '../../Services/api-persona.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-listar-personas',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './listar-personas.component.html',
   styleUrl: './listar-personas.component.css'
 })
 export class ListarPersonasComponent {
 
   constructor(private router: Router, private apiService: ApiPersonaService) {}
-  data: any;
+  dataPersonas: any;
 
 
   ngOnInit(): void {
@@ -32,11 +33,18 @@ export class ListarPersonasComponent {
   fetchData(): void {
     this.apiService.getData('listar') 
       .subscribe(response => {
-        this.data = response;
-        console.log(this.data);
+        this.dataPersonas = response;
+        console.log(this.dataPersonas);
       }, error => {
         console.error('Error fetching data:', error);
       });
+  }
+
+
+  editPersona(persona: any) {
+    // Your logic to handle the editing of the selected persona
+    console.log('Editing persona:', persona);
+    // For example, you might open a modal or navigate to an edit form
   }
 
 }
